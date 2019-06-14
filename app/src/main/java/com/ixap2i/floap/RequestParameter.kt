81bf788d13a3b314024ce4bc1dea29be21d59315
ixap2i <ayamashita124@gmail.com>
2019-06-14 23:03:21 +0900

    add paramter or enum

--- app/src/main/java/com/ixap2i/floap/RequestParameter.kt
+++ app/src/main/java/com/ixap2i/floap/RequestParameter.kt
@@ -1,2 +1,28 @@
 package com.ixap2i.floap
 
+import io.ktor.http.HttpStatusCode
+import io.netty.handler.codec.http.HttpResponse
+
+interface RequestParameter {
+    fun toFilterParams(): List<Pair<String, String>>
+}
+
+val FILTERED_VAL get() = "[FILTERED]"
+
+
+
+
+interface ImageResponse {
+    val phoneNumber: String
+    val accessKey: String
+    val confirmed: Boolean
+}
+
+enum class ImageErrorResponse {
+    PHONE_NUMBER_INVALID, PHONE_NUMBER_REGISTERED
+}
+
+class BadResponseStatusException(
+    val statusCode: HttpStatusCode,
+    val response: HttpResponse
+) : IllegalStateException("Received bad status code: $statusCode. Expected status code < 300.")
