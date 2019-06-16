9c36c727f30d7124d41ce111a2fa1b934f1885dd
ixap2i <ayamashita124@gmail.com>
2019-06-16 23:34:34 +0900

    add dependency injection

--- app/src/main/java/com/ixap2i/floap/FloapApplication.kt
+++ app/src/main/java/com/ixap2i/floap/FloapApplication.kt
@@ -21,26 +21,5 @@ class FloapApplication: Application() {
 }
 
 val appModule = module {
-    single<FloapApplication> { FloapApplication() }
-    single { ImageServiceImpl(androidContext(), floapApi = get()) }
+    single { FloapApplication() }
 }
-//fun Application.main() {
-//    install(DefaultHeaders)
-//    install(CallLogging)
-//    install(ContentNegotiation) {
-//        gson {
-//            setPrettyPrinting()
-//        }
-//    }
-//}
-//fun Application.routes() {
-//    val service: ImageService by inject()
-//
-//   routing {
-//       route("https://api.instagram.com/oauth/authorize/?client_id=185bc9b4b55a4afe9922aed4db639b2b&redirect_uri=https://www.yahoo.co.jp/&response_type=code") {
-//           get  {
-//               call.respondText(service.getCode())
-//           }
-//        }
-//    }
-//}
