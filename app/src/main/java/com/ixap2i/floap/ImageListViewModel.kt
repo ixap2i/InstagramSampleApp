2e461026105ffa671bf42b670cb4c44c62558789
ixap2i <ayamashita124@gmail.com>
2019-06-16 23:05:30 +0900

    pending list view model

--- app/src/main/java/com/ixap2i/floap/ImageListViewModel.kt
+++ app/src/main/java/com/ixap2i/floap/ImageListViewModel.kt
@@ -2,12 +2,8 @@ package com.ixap2i.floap
 
 import android.app.Application
 import androidx.lifecycle.AndroidViewModel
-import androidx.lifecycle.LiveData
 
 class ImageListViewModel(application: Application) : AndroidViewModel(application) {
-
-//    private val imageListObserval: LiveData<List<ImageRepository>>
-//    = ImageRepository().getInstance()
 
     override fun <T : Application> getApplication(): T {
         return super.getApplication()
